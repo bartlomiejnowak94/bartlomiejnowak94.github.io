66b4e607ba7e3567c485d99d7e6e412614c95107
Bartek Nowak Dom <barteknowak9418@gmail.com>
2023-02-21 13:16:09 +0100

    presentations

--- js/menuhighlight.js
+++ js/menuhighlight.js
@@ -1,31 +1,20 @@
-// Get all sections that have an ID defined
-const sections = document.querySelectorAll("section[id]");
+document.addEventListener("DOMContentLoaded", () => {
+  section = document.querySelectorAll('section');
+  navLinks = document.querySelectorAll('header nav .navbar-links ul li a');
 
-// Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
+  window.onscroll = () =>{
+    section.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
 
-function navHighlighter() {
-  
-  // Get current scroll position
-  let scrollY = window.pageYOffset;
-  
-  // Now we loop through sections to get height, top and ID values for each
-  sections.forEach(current => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-    
-    /*
-    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-    */
-    if (
-      scrollY > sectionTop &&
-      scrollY <= sectionTop + sectionHeight
-    ){
-      document.querySelector(".navbar a[href*=" + sectionId + "]").classList.add("current");
-    } else {
-      document.querySelector(".navbar a[href*=" + sectionId + "]").classList.remove("current");
-    }
-  });
-}
+      if (top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('current');
+          document.querySelector('header nav .navbar-links ul li a[href*=' + id + ']').classList.add('current');
+        });
+      };
+    });
+  };
+});
